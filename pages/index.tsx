a759642dc915c5dae3445fc39d89c740314cb840
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-11-03 11:21:22 +0100

    paimon

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import { PaddingScheme, Paimon, RsaPrivateKey } from "@hazae41/paimon";
+
+async function test() {
+  await Paimon.initBundledOnce();
+
+  const keypair = new RsaPrivateKey(1024);
+  console.log("keypair.to_pkcs1_der", keypair.to_pkcs1_der());
+  console.log("keypair.to_pkcs8_der", keypair.to_pkcs8_der());
+
+  const identity = keypair.to_public_key();
+  console.log("identity.to_pkcs1_der", identity.to_pkcs1_der());
+  console.log("identity.to_public_key_der", identity.to_public_key_der());
+
+  const bytes = new TextEncoder().encode("hello world");
+  console.log("bytes", bytes);
+
+  const padding = PaddingScheme.new_pkcs1v15_sign_raw();
+
+  const proof = keypair.sign(padding, bytes);
+  console.log("proof", proof);
+
+  const verified = identity.verify(padding, bytes, proof);
+  console.log("verified", verified);
+}
+
+export default function Page() {
+  return <>
+    Open browser console and <button onClick={test}>click me</button>
+  </>
+}
